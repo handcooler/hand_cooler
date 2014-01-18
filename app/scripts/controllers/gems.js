6f1ab1a0b06cc3720a61f0ac3babbdae608ecfe5
sanemat <o.gata.ken@gmail.com>
2014-01-18 17:17:00 +0900

    Fix typo

--- app/scripts/controllers/gems.js
+++ app/scripts/controllers/gems.js
@@ -2,7 +2,7 @@
 
 angular.module('handCoolerApp')
   .controller('GemsCtrl', function ($scope, $http) {
-    $http.get('gems/gems-latest.jspn').success(function(data) {
+    $http.get('gems/gems-latest.json').success(function(data) {
       $scope.gems = data;
     });
   });
