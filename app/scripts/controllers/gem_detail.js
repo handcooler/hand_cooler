194bd76a05f252b191b31c22f3e7a5542694b700
sanemat <o.gata.ken@gmail.com>
2014-01-19 02:20:53 +0900

    Use api

--- app/scripts/controllers/gem_detail.js
+++ app/scripts/controllers/gem_detail.js
@@ -3,7 +3,11 @@
 angular.module('handCoolerApp')
   .controller('GemDetailCtrl', function ($scope, $http, $routeParams) {
     $scope.gemName = $routeParams.gemName;
-    $http.get('gems/tachikoma.json').success(function(data) {
+//    $http.get('gems/tachikoma.json').success(function(data) {
+//      $scope.detail = data;
+//    });
+    var gemApi = 'https://rubygems.org/api/v1/gems/' + $routeParams.gemName + '.json';
+    $http.get(gemApi).success(function(data) {
       $scope.detail = data;
     });
   });
