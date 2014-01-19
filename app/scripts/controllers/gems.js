37c44ed57884487377d9cb6831355118b0096795
sanemat <o.gata.ken@gmail.com>
2014-01-20 00:36:07 +0900

    Add search query

--- app/scripts/controllers/gems.js
+++ app/scripts/controllers/gems.js
@@ -1,5 +1,11 @@
 'use strict';
 
 angular.module('handCoolerApp')
-  .controller('GemsCtrl', function () {
+  .controller('GemsCtrl', function ($scope, $http, $routeParams) {
+    $scope.pageCount = $routeParams.page || 1;
+    $scope.query = $routeParams.query;
+    var searchApi = 'http://cornflower.herokuapp.com/rubygems.org/api/v1/search.json?query=' + $routeParams.query + '&page=' + $scope.pageCount;
+    $http.get(searchApi).success(function(data) {
+      $scope.gems = data;
+    });
 });
