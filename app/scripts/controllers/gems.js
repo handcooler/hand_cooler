96c6d64c0916fa0ab01bc4d6c551596eb66601f8
sanemat <o.gata.ken@gmail.com>
2014-01-19 22:14:17 +0900

    Remove no use file

--- app/scripts/controllers/gems.js
+++ app/scripts/controllers/gems.js
@@ -1,8 +1,5 @@
 'use strict';
 
 angular.module('handCoolerApp')
-  .controller('GemsCtrl', function ($scope, $http) {
-    $http.get('gems/gems-latest.json').success(function(data) {
-      $scope.gems = data;
-    });
-  });
+  .controller('GemsCtrl', function () {
+});
