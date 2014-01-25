4fd6696a00df53bc99f918ba7d3d3c0a9e6c0d1b
sanemat <o.gata.ken@gmail.com>
2014-01-25 21:41:28 +0900

    Detect github uri

--- app/scripts/controllers/gem_detail.js
+++ app/scripts/controllers/gem_detail.js
@@ -6,8 +6,19 @@ angular.module('handCoolerApp')
 //    $http.get('gems/tachikoma.json').success(function(data) {
 //      $scope.detail = data;
 //    });
+    $scope.detectRepos = function(data) {
+      if(data.source_code_uri && URI(data.source_code_uri).hostname() === 'github.com') {
+        return data.source_code_uri;
+      } else if(data.homepage_uri && URI(data.homepage_uri).hostname() === 'github.com') {
+        return data.homepage_uri;
+      } else {
+//        search from github api?
+        return false;
+      }
+    };
     var gemApi = 'http://cornflower.herokuapp.com/rubygems.org/api/v1/gems/' + $routeParams.gemName + '.json';
     $http.get(gemApi).success(function(data) {
       $scope.detail = data;
+      $scope.sourceUrl = $scope.detectRepos(data);
     });
   });
