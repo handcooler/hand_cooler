70c4738379eae5d8242c31aed89b718fa649bbab
sanemat <o.gata.ken@gmail.com>
2014-02-08 21:52:00 +0900

    Use urijs

--- app/scripts/controllers/gems.js
+++ app/scripts/controllers/gems.js
@@ -5,7 +5,8 @@ angular.module('handCoolerApp')
     $scope.pageCount = $routeParams.page || 1;
     $scope.query = $routeParams.query;
     $scope.doSearch = function () {
-      var searchApi = 'http://cornflower.herokuapp.com/rubygems.org/api/v1/search.json?query=' + $scope.query + '&page=' + $scope.pageCount;
+      var searchApi = new URI('http://cornflower.herokuapp.com/rubygems.org/api/v1/search.json');
+      searchApi.search({ query: $scope.query, page: $scope.pageCount });
       $http.get(searchApi).success(function(data) {
         $scope.gems = data;
       });
