4152f7934e2a7f427f5751f74edfd98f313b284f
sanemat <o.gata.ken@gmail.com>
2014-02-09 12:02:32 +0900

    Show original response in error

--- test/spec/controllers/gems.js
+++ test/spec/controllers/gems.js
@@ -26,10 +26,10 @@ describe('Controller: GemsCtrl', function () {
   it('should adjust invalid response', function(){
     var uri = 'http://cornflower.herokuapp.com/rubygems.org/api/v1/search.json?query=formatter&page=1';
     var routeParams = { query: 'formatter' };
-    httpBackend.expectGET(uri).respond(404, '');
+    httpBackend.expectGET(uri).respond(404, 'Not Found');
     ctrl = controllerService('GemsCtrl', { $scope: scope, $routeParams: routeParams });
     httpBackend.flush();
-    expect(scope.gems).toBe('Request Failed');
+    expect(scope.gems).toBe('Not Found');
   });
 
   it('should return response', function(){
