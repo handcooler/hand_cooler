66865da516ca2a7709d186b7a7ceac257a59784b
sanemat <o.gata.ken@gmail.com>
2014-02-09 10:38:42 +0900

    Move routeParams to test params

--- test/spec/controllers/gems.js
+++ test/spec/controllers/gems.js
@@ -11,17 +11,17 @@ describe('Controller: GemsCtrl', function () {
     ctrl;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope, $httpBackend, $routeParams) {
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
     scope = $rootScope.$new();
     controllerService = $controller;
     httpBackend = $httpBackend;
-    $routeParams.query = 'tachikoma';
   }));
 
   it('should adjust invalid response', function(){
-    var uri = 'http://cornflower.herokuapp.com/rubygems.org/api/v1/search.json?query=tachikoma&page=1';
+    var uri = 'http://cornflower.herokuapp.com/rubygems.org/api/v1/search.json?query=formatter&page=1';
+    var routeParams = { query: 'formatter' };
     httpBackend.expectGET(uri).respond(404, '');
-    ctrl = controllerService('GemsCtrl', { $scope: scope });
+    ctrl = controllerService('GemsCtrl', { $scope: scope, $routeParams: routeParams });
     httpBackend.flush();
     expect(scope.gems).toBe('Request Failed');
   });
